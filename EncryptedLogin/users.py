35bf99fb5a37bafe7ad37e39d98360f2fe331a6f
joaokienen <42853031+joaokienen@users.noreply.github.com>
2021-10-04 15:17:45 -0300

    Create users.py

---
+++ EncryptedLogin/users.py
@@ -0,0 +1,31 @@
+usuarios = []
+
+usuario = {
+
+    "name": "Clark Kent",
+
+    "username" : "kent",
+
+    "password" : "6c70795dc50a2d6765a44bceda2699c48a8e81effe3f9e7cf3b6f8bb34ccb5b2"
+
+}
+
+usuario = {
+
+    "name": "Bruce Wayne",
+
+    "username" : "wayne",
+
+    "password" : "0bf3116d14ceeb7b8859abb9f5b90a7747913185930fed774a949cc8777a990a"
+
+}
+
+usuario = {
+
+    "name": "Christopher Walker",
+
+    "username" : "walker",
+
+    "password" : "db7e65b576f6b3db9041cbddff92f9a4b7253b795aab817d3f348977b014cd83"
+
+}
