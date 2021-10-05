ce8bf267af71d66eea76b2a6ed57c84b8c15322a
JvkScript <42853031+joaokienen@users.noreply.github.com>
2021-10-05 09:01:58 -0300

    Update LoginHash.py

--- ProjectLoginHash/LoginHash.py
+++ ProjectLoginHash/LoginHash.py
@@ -18,12 +18,12 @@ senha = input("Digite sua senha: ")
 
 senha = (sha256(senha.encode('utf-8')).hexdigest())
 
-for i in Users:
+for i in usuarios:
   if username in i['username'] == username and senha in i['password'] == senha:
     x = x + 1
     print("{} logado!".format(i['name']))
 
-for i in Users:
+for i in usuarios:
   if (username not in i['username'] or senha not in i['password']):
     while (x<1):
       print("Usuário/Senha inválidos!")
