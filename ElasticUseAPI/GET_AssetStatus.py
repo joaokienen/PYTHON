94f10af53068582d9935050f48e4fe0faad2ae89
JvkScript <42853031+joaokienen@users.noreply.github.com>
2023-05-24 17:14:12 -0300

    Add files via upload

---
+++ ElasticUseAPI/GET_AssetStatus.py
@@ -0,0 +1,48 @@
+# Importa a classe Elasticsearch do módulo elasticsearch
+from elasticsearch import Elasticsearch
+
+# Cria uma conexão com o Elasticsearch
+es = Elasticsearch(
+    ['https://cybersec.domain.com.br:9200/.fleet-agents/'],
+    http_auth=('user', 'pass'),
+    verify_certs=False
+)
+
+# Define host de consulta
+host = 'cpanel09-idc.domain.com.br'
+
+# Define a consulta
+query = {
+  "_source": ["last_checkin_status"],
+  "size": 10,
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "term": {
+            "local_metadata.host.hostname.keyword": {
+              "value": f"{host}"
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+
+# Realiza a consulta
+response = es.search(body=query)
+
+# itera sobre os resultados
+for hit in response['hits']['hits']:
+    source = hit['_source']
+    # processa os campos retornados
+    status = source.get('last_checkin_status')
+    print(f"Status: {status}\n")
+
+
+
+
+
+
+
