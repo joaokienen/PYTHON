71b4d188f04ac10d5bc87a370b741f6a041f23ee
JvkScript <42853031+joaokienen@users.noreply.github.com>
2023-05-25 13:05:45 -0300

    Add files via upload

--- TenableUseAPI/GET_AssetStatus.py
+++ TenableUseAPI/GET_AssetStatus.py
@@ -1,3 +1,5 @@
+#################################### Parte 1: Obtendo [ last_seen ] de um agente ->
+
 # Realiza as importações necessárias
 import requests
 from datetime import datetime, timedelta
@@ -11,7 +13,7 @@ url = f"https://cloud.tenable.com/workbenches/assets?filter.0.filter=hostname&fi
 # Declara a autorização para acessar a url de destino
 headers = {
     "accept": "application/json",
-    "X-ApiKeys": "accessKey=0000000000000;secretKey=0000000000000;"
+    "X-ApiKeys": "accessKey=XXXXXXXXXX;secretKey=XXXXXXXXXXXX;"
 }
 
 # Recupera resposta
@@ -22,6 +24,10 @@ data = response.json()
 
 # Obtem campo necessário da resposta anterior
 last_seen = data['assets'][0]['last_seen']
+
+
+
+#################################### Parte 2: Validando se agente está ativo com base no [ last_seen ] ->
 
 # Declara função de tempo, para verificar se host está ativo ou inativo
 def is_more_than_10_days(last_seen):
@@ -37,14 +43,10 @@ def is_more_than_10_days(last_seen):
 
     # Verifica se a diferença é maior que 10 dias
     if diff > timedelta(days=10):
-        return "Offline"
+        return "offline"
     else:
-        return "Online"
+        return "online"
 
 # Recupera status final
 result = is_more_than_10_days(last_seen)
 print(result)
-
-
-
-
