9e64c309e39fc27fbeb369165bc8ea29addae73c
JvkScript <42853031+joaokienen@users.noreply.github.com>
2024-01-03 14:00:33 -0300

    Create GET_Teams.py

---
+++ AceleratoUseAPI/GET_Teams.py
@@ -0,0 +1,28 @@
+# Realiza a importação de módulos necessários
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+# -------->
+
+# Define URL de chamada
+url = "http://atendimento.acelerato.com/api/publica/equipes"
+
+# Declara credenciais de autenticação
+from dotenv import load_dotenv
+load_dotenv('./.env')
+
+mail = os.getenv('MAIL')
+token = os.getenv('TOKEN')
+
+
+# -------->
+
+# Define headers
+headers = {
+'Content-Type': 'application/json; charset=utf-8'
+}
+
+# Realiza a requisição GET com autenticação básica
+response = requests.get(url, headers=headers, auth=HTTPBasicAuth(mail, token))
