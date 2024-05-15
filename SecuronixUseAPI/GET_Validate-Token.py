a08e21e717990469fc5ca9898e0eae64833f705c
JvkScript <42853031+joaokienen@users.noreply.github.com>
2024-05-15 17:25:17 -0300

    Update GET_Validate-Token.py

--- SecuronixUseAPI/GET_Validate-Token.py
+++ SecuronixUseAPI/GET_Validate-Token.py
@@ -1,33 +1,22 @@
 # https://documentation.securonix.com/bundle/securonix-cloud-user-guide/page/content/authentication.htm
 
+# Example .env tokens:
+BASE_URL = os.getenv('BASE_URL') 
+TOKEN = os.getenv('TOKEN') 
+
 # Imports necessary modules
 import os
 import requests
 
-def validate_token(BASE_URL, token):
-    # Construct the URL for token validate
+def validate_token(BASE_URL, token):=
     url = f"{BASE_URL}/ws/token/validate"
     
-    # Set headers for the request including username, password, and validity of the token
     headers = {
         "token": token
     }
     
-    # Send a GET request to the constructed URL with the headers
     response = requests.get(url, headers=headers)
-
-    # Print the response content
     print(response.text)
-    
-    # Check if the response status code indicates success (200)
-    if response.status_code == 200:
-        print("Valid token.")   # Print message if token is valid
-    else:
-        print(f"Invalid token. Status code: {response.status_code}")  # Print message if token is invalid along with status code
 
-# Example usage:
-BASE_URL = os.getenv('BASE_URL')  # Get the base URL from environment variables
-TOKEN = os.getenv('TOKEN')        # Get the token from environment variables
-
-# Call the function to generate a token
+# Call the function to validate a token
 validate_token(BASE_URL, TOKEN)
