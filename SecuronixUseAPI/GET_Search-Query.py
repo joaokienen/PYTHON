ca53ea82e87fa3cb9e8885ed9b8a920abe18e755
JvkScript <42853031+joaokienen@users.noreply.github.com>
2024-05-31 13:25:10 -0300

    Update GET_Search-Query.py

--- SecuronixUseAPI/GET_Search-Query.py
+++ SecuronixUseAPI/GET_Search-Query.py
@@ -1,13 +1,24 @@
 # https://documentation.securonix.com/bundle/securonix-cloud-user-guide/page/content/authentication.htm
 
-# Example .env tokens:
-BASE_URL = os.getenv('BASE_URL') 
-TOKEN = os.getenv('TOKEN') 
-
 # Imports necessary modules
 import os
 import requests
 
+
+# ---> Config
+current_dir = os.path.dirname(os.path.realpath(__file__))
+env_path = os.path.join(current_dir, f'./.env')
+
+from dotenv import load_dotenv
+load_dotenv(env_path)
+
+# Example .env tokens:
+BASE_URL = os.getenv('BASE_URL') 
+TOKEN = os.getenv('TOKEN')
+# --->
+
+
+# ---> Function request
 def search_spotter(BASE_URL, token):
     url = f"{BASE_URL}/ws/spotter/index/search"
     
@@ -22,7 +33,9 @@ def search_spotter(BASE_URL, token):
     }
     
     response = requests.get(url, params=query_parameters, headers=headers)
-    print(response.text)
+    # print(response.text)
+
+# ---> 
 
 # Call the function to get query results
 search_spotter(BASE_URL, TOKEN)
