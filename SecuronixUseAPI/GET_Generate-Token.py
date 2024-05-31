0eaa15f580e9d2f8c74ce3a7afd2579257b35715
JvkScript <42853031+joaokienen@users.noreply.github.com>
2024-05-31 13:25:01 -0300

    Update GET_Generate-Token.py

--- SecuronixUseAPI/GET_Generate-Token.py
+++ SecuronixUseAPI/GET_Generate-Token.py
@@ -1,16 +1,26 @@
 # https://documentation.securonix.com/bundle/securonix-cloud-user-guide/page/content/authentication.htm
 
-# Example .env tokens:
-BASE_URL = os.getenv('BASE_URL') 
-TOKEN = os.getenv('TOKEN') 
-USER_ID = os.getenv('USER_ID') 
-PASS_ID = os.getenv('PASS_ID') 
-
 # Imports necessary modules
 import os
 import requests
 
-def generate_token(BASE_URL, token):
+
+# ---> Config
+current_dir = os.path.dirname(os.path.realpath(__file__))
+env_path = os.path.join(current_dir, f'./.env')
+
+from dotenv import load_dotenv
+load_dotenv(env_path)
+
+# Example .env tokens:
+BASE_URL = os.getenv('BASE_URL') 
+USER_ID = os.getenv('USER_ID') 
+PASS_ID = os.getenv('PASS_ID') 
+# --->
+
+
+# ---> Function request
+def generate_token(BASE_URL):
     url = f"{BASE_URL}/ws/token/generate"
     
     headers = {
@@ -20,7 +30,9 @@ def generate_token(BASE_URL, token):
     }
     
     response = requests.get(url, headers=headers)
-    print(response.text)
+    # print(response.text)
+
+# --->
 
 # Call the function to generate a token
-generate_token(BASE_URL, TOKEN)
+generate_token(BASE_URL)
