14729b77372d554350778c7aeef7685b83c43c80
JvkScript <42853031+joaokienen@users.noreply.github.com>
2024-05-31 13:25:20 -0300

    Update GET_Validate-Token.py

--- SecuronixUseAPI/GET_Validate-Token.py
+++ SecuronixUseAPI/GET_Validate-Token.py
@@ -1,14 +1,25 @@
 # https://documentation.securonix.com/bundle/securonix-cloud-user-guide/page/content/authentication.htm
 
-# Example .env tokens:
-BASE_URL = os.getenv('BASE_URL') 
-TOKEN = os.getenv('TOKEN') 
-
 # Imports necessary modules
 import os
 import requests
 
-def validate_token(BASE_URL, token):=
+
+# ---> Config
+current_dir = os.path.dirname(os.path.realpath(__file__))
+env_path = os.path.join(current_dir, f'./.env')
+
+from dotenv import load_dotenv
+load_dotenv(env_path)
+
+# Example .env tokens:
+BASE_URL = os.getenv('BASE_URL') 
+TOKEN = os.getenv('TOKEN')
+# --->
+
+
+# ---> Function request
+def validate_token(BASE_URL, token):
     url = f"{BASE_URL}/ws/token/validate"
     
     headers = {
@@ -16,7 +27,9 @@ def validate_token(BASE_URL, token):=
     }
     
     response = requests.get(url, headers=headers)
-    print(response.text)
+    # print(response.text)
+
+# --->
 
 # Call the function to validate a token
 validate_token(BASE_URL, TOKEN)
